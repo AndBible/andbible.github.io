e56e8a676f89b2133670e54e44728bd4ca7f50d5
BryceSteuer <48736452+BryceSteuer@users.noreply.github.com>
2021-09-07 16:15:24 -0600

    Counter, new feature list, screenshots

---
+++ javascripts/counter.js
@@ -0,0 +1,26 @@
+// number count for stats, using jQuery animate
+
+$('.counting').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 3000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+
+});
